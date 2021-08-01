a2b92b6e6ec38a157f3bbd0d95284a96240a2e56
Hüseyin Emre Kaya <h.emrekaya2207@gmail.com>
2021-08-02 01:41:32 +0300

    last

--- src/main/java/com/example/permissionControl/Persons/persons.java
+++ src/main/java/com/example/permissionControl/Persons/persons.java
@@ -1,5 +1,6 @@
 package com.example.permissionControl.Persons;
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,16 +9,30 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "persons")
-@NoArgsConstructor
 @Getter
 @Setter
 public class persons {
+
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
     private String phone;
     private String city;
     private String gender;
+
+
+    public persons(Integer id, String name, String email, String phone, String city, String gender) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.city = city;
+        this.gender = gender;
+    }
+
+    public persons() {
+    }
+
 }
--- src/main/java/com/example/permissionControl/controllers/personsController.java
+++ src/main/java/com/example/permissionControl/controllers/personsController.java
@@ -1,5 +1,6 @@
 package com.example.permissionControl.controllers;
 
+import com.example.permissionControl.DB.App;
 import com.example.permissionControl.Persons.persons;
 import com.example.permissionControl.personsRepository.personsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
 import javax.xml.ws.Response;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,17 +22,17 @@ public class personsController {
     private personsRepository perRepo;
 
     @PostMapping
-    public ResponseEntity<?> addNewPerson(@RequestBody persons person1){
-        perRepo.save(person1);
-        return ResponseEntity.ok(person1);
+    public ResponseEntity<?> addNewPerson(@RequestBody persons person1) throws SQLException {
+        App insertP = new App();
+        return ResponseEntity.ok(insertP.insertPerson(person1));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> getPersonById(@PathVariable Integer id){
 
         try {
-            persons person = perRepo.findById(id).get();
-            return ResponseEntity.ok(person);
+            App find = new App();
+            return ResponseEntity.ok(find.findPersonByID(id));
         } catch (Exception e){
             return ResponseEntity.status(404).body(null);
         }
@@ -38,15 +40,22 @@ public class personsController {
     }
 
     @GetMapping
-    public Iterable<persons> getPersons(
+    public List<persons> getPersons(
            @RequestParam(name = "_page", defaultValue = "1") Integer pageNum,
            @RequestParam(name = "_limit", defaultValue = "10") Integer pageSize)
     {
-
+        App app = new App();
         Pageable p = PageRequest.of(pageNum-1,pageSize);
 
-        return perRepo.findAll(p).getContent();
+        return app.getPersons();
     }
 
+    @DeleteMapping
+    public persons deletePersons(
+            @RequestParam(name = "_del", defaultValue = "104") Integer id) throws SQLException {
+        App delP = new App();
+        delP.deletePersons(id);
+        return delP.findPersonByID(id);
+    }
 
 }
